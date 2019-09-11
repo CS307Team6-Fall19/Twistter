52377307e5aa1c985f14ff7b856d020f7945e372
Rohan Gupta <gupta326@purdue.edu>
2019-09-11 17:38:19 -0400

    cleaned repo

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './firebase-auth-tutorial/src/serviceWorker';
 
-// var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
-// ReactDOM.render(element, document.getElementById('root'));
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
