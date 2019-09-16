c65d58edc43b4d44e18bc5aa5e26dd5068814bad
Gosseling de Vries <gdevrie@purdue.edu>
2019-09-15 23:04:43 -0400

    Landing now shows logged in user's email

--- twisster/src/App.js
+++ twisster/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Home";
 import Login from "./LogIn";
 import SignUp from "./SignUp";
-import landing from "./Landing";
+import Landing from "./Landing";
 import { BrowserRouter as Link} from "react-router-dom";
 
 
--- twisster/src/Landing.js
+++ twisster/src/Landing.js
@@ -6,52 +6,48 @@ import ReactDOM from 'react-dom';
 import firebase from "firebase";
 
 class Landing extends Component {
-    constructor(props){
-        super(props)
 
-        firebase.auth().onAuthStateChanged(function(user) {
-          if (user) {
-            console.log("user is not null");
-            console.log("user id: " + user.uid);
-            document.getElementById('email').innerHTML = user.email;
-          } else {
-            console.log("user is null");
-          }
-        });
+  componentDidMount () {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        console.log("user is not null");
+        console.log("user id: " + user.uid);
+        document.getElementById('email').innerHTML = user.email;
+      } else {
+        console.log("user is null");
+      }
+    });
+  }
 
-        /* this.useruid = this.useruid.bind(this);
-        firebase.auth().onAuthStateChanged(function(user) {
-          this.useruid = user.uid;
-        });   */
-        this.renderClock();
-    }
-    
-    render() {
-      return(
-        <div>
-        {this.renderWelcome()}
-        
-        </div>
-      );
-    }
+  constructor(props){
+      super(props)
+  }
+  
+  render() {
+    return(
+      <div>
+      {this.renderWelcome()}
+      </div>
+    );
+  }
 
-    renderClock(){
-      ReactDOM.render(
-        <Clock />,
-        document.getElementById('root')
-      );
-    }
+  renderClock(){
+    ReactDOM.render(
+      <Clock />,
+      document.getElementById('root')
+    );
+  }
 
-    renderWelcome = () => {
-      return(
-        <div>
-          <h1>Welcome to Twistter!</h1>
-          <form>
-          <label>hello</label>
-          </form>
-        </div>
-      );
-    }
+  renderWelcome = () => {
+    return(
+      <div>
+        <h1>Welcome to Twistter!</h1>
+        <form>
+        <label id='email'>hello</label>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default withRouter(Landing);
