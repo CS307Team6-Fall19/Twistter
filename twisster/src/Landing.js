2eba5f5fd230384fe2b1ff00d87163ef38345501
Benjamin Loisch <ben.loisch@gmail.com>
2019-09-15 22:24:38 -0400

    fixed landing not showing and added auth for checking logged in user email and displays it

--- twisster/src/Landing.js
+++ twisster/src/Landing.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
 import app from "./base";
+import firebase from "firebase";
 
 class Landing extends Component {
     constructor(props){
         super(props)
         this.user = props.loggedInUser
+
+        firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+            console.log("user is not null");
+            console.log("user id: " + user.uid);
+            document.getElementById('email').innerHTML = user.email;
+          } else {
+            console.log("user is null");
+          }
+        });
     }
     
     render() {
@@ -13,7 +24,7 @@ class Landing extends Component {
           <div>
             <h1>Landing</h1>
             <form>
-            <label>this.user.email</label>
+            <label id='email'>this.user.email</label>
             </form>
           </div>
       );
