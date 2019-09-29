cf6aaaed980475ca4ede8d3a6d198817b8732eec
Gosseling de Vries <gdevrie@purdue.edu>
2019-09-29 16:09:47 -0400

    Top bar is its own component

---
+++ twisster/src/TopBar/TopBarView.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './TopBar.css'
+
+const TopBarTwisster = (/* {onClickProfile} */) => {
+    return (
+      <div className="inner-body">
+        <label className="top-bar-twisster" >Twistter</label>
+        {/* <input className="top-bar-search" placeholder="Search"/>
+        <button className="top-bar-button" type="button" onClick={onClickProfile}>Go to your profile</button>  */}   
+      </div>
+    )
+}
+export default TopBarTwisster;
