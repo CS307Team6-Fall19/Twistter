5e898cc5266c2ce118534f331fca60c3ef68e7da
Gosseling de Vries <gdevrie@purdue.edu>
2019-09-30 21:33:59 -0400

    Top bar has logout button

--- twisster/src/TopBar/TopBarView.js
+++ twisster/src/TopBar/TopBarView.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './TopBar.css'
 
-const TopBarTwisster = (/* {onClickProfile} */) => {
+const TopBarTwisster = ({onClick}) => {
     return (
       <div className="inner-body">
         <label className="top-bar-twisster" >Twistter</label>
+        <button className="top-bar-logout" onClick={onClick}>Logout</button>
         {/* <input className="top-bar-search" placeholder="Search"/>
         <button className="top-bar-button" type="button" onClick={onClickProfile}>Go to your profile</button>  */}   
       </div>
--- twisster/src/TopBar/index.js
+++ twisster/src/TopBar/index.js
@@ -1,19 +1,39 @@
 import TopBarTwisster from './TopBarView'
 import React, { PureComponent } from 'react'
+import { withRouter } from "react-router";
 
+import firebase from "firebase";
 class TopBar extends React.Component{
 
     constructor(props) {
   
       super(props);
+
+      this.goLogout = this.goLogout.bind(this);
     }
+
+    goLogout = async event => {
+      firebase.auth().signOut()
+        .then(function() {
+          console.log("Signout succesful");
+
+          
+        })
+        .catch(function(error) {
+          console.log("Error");
+        })
+  
+        this.props.history.push({
+          pathname: "/login"
+        })
+    };
 
     render(){
         return(
             <div className="top-bar">
-                <TopBarTwisster /* onClickProfile={onClick} */ />
+                <TopBarTwisster onClick={this.goLogout} />
             </div>
           )
     }
 }
-export default TopBar;
+export default withRouter(TopBar);
