4eb8c3fe12ec6c1865bbb75cca7d07bd62ddd581
Gosseling de Vries <gdevrie@purdue.edu>
2019-10-13 16:25:31 -0400

    Microblog component for rendering one microblog

--- twisster/src/Microblog/index.js
+++ twisster/src/Microblog/index.js
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
-import MicroblogView from './MicroblogView';
+import { MicroblogView } from './MicroblogView.js';
+
 class Microblog extends React.Component{
 
     constructor(props) {
       super(props);
 
-      this.name = props.name;
-      this.handle = props.handle;
-      this.tweet = props.tweet;
-      this.image = props.image;
-      this.topics = props.topics;
+      this.microblogData = new Object();
+
+
+      this.microblogData.name = props.username;
+      this.microblogData.handle = props.username;
+      this.microblogData.tweet = props.data.content;
+      this.microblogData.image = "props.image";
+      this.microblogData.topics = "props.topics";
+      
     }
 
-    render(){
+    /* render(){
         return(
             <MicroblogView 
-                key={this.index}
                 name={this.name}
                 handle={this.handle}
                 tweet={this.tweet}
                 image={this.image} 
                 topics={this.topics}
             />
+        );
+    } */
+    render(){
+        let name = `${this.microblogData.name}`
+        let handle = `@${this.microblogData.name}`
+        let image = this.microblogData.image
+        let tweet = this.microblogData.tweet
+        //let topics = user.topics
+        return(
+            
+            <MicroblogView 
+                /* name={this.microblogData.name} 
+                handle={this.microblogData.same}
+                tweet={this.microblogData.tweet}
+                image={this.microblogData.image}
+                topics={this.microblogData.topics} */
+                key={this.microblogData.key}
+                name={name}
+                handle={handle}
+                image={image}
+                tweet={tweet}
+                //props={this.microblogData}
+            /> 
         );
     }
 }
