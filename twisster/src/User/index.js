0a765268cdaf7a397ce85450077611c43f8545c7
Rohan Gupta <gupta326@purdue.edu>
2019-10-20 19:21:08 -0400

    should compile now with file change

--- twisster/src/User/index.js
+++ twisster/src/User/index.js
@@ -36,138 +36,16 @@ class User extends React.Component{
     }
 
     editProfile(){
-
-<<<<<<< HEAD
         this.editMode = true;
         this.setState({
           editMode : 1
         })
     }
-=======
-    this.editProfile = this.editProfile.bind(this);
-    this.saveChanges = this.saveChanges.bind(this);
-    this.setNewBio = this.setNewBio.bind(this);
-    this.followUserIAmViewing = this.followUserIAmViewing.bind(this);
-    this.getFollowedTopics();
-    //this.getMicroblogs = this.getMicroblogs.bind(this);
->>>>>>> rohangupta
     
     saveChanges(){
     
-<<<<<<< HEAD
         var bio_cont = document.getElementById('bioTextBox').value;
         helperfunctions.postCurrentUserBio(bio_cont);
-=======
-    this.setState({
-      users:[
-        {
-          name: nameInput,//data.results[0].name,
-          image: "",//data.results[0].picture.medium,
-          tweet: tweetInput,
-          topics: topicsInput
-        },
-        ...this.state.users,
-      ]
-    });
-  }  
-
-  getUsername(){
-      return this.username;
-  }
-
-  async getBio(username) {
-
-    var bio_text;
-
-    await firebase.database().ref().once('value', (snapshot) => {
-      var user_uid_list = snapshot.child('mapUsernameToUID').val();
-      var uid_val = user_uid_list[username];
-      console.log(user_uid_list);
-      console.log(uid_val);
-      var bio_cont = snapshot.child("users").child(uid_val).child("bio").val();
-      console.log("bio_cont: ", bio_cont);
-      if (bio_cont != undefined) {
-        this.bio.setText(bio_cont);
-        bio_text = bio_cont;
-      }
-    });
-
-    document.getElementById('bio').innerHTML = this.bio.getText();
-  }
-
-  getUid(){
-    return this.uid;
-  }
-
-  editProfile(){
-    this.editMode = true;
-    this.setState({
-      editMode : 1
-    })
-  }
-
-  saveChanges(){
-
-    this.setNewBio();
-    document.getElementById('bio').innerHTML = this.getBio();
-    this.editMode = false;
-    this.setState({
-      editMode : 0
-    })
-  }
-
-  setNewBio(){ 
-    var bio_cont = document.getElementById('bioTextBox').value;
-    helperfunctions.postCurrentUserBio(bio_cont);
-    this.bio.setText(bio_cont);
-  }
-
-  getFollowersAndFollowing() {
-    firebase.database().ref().once('value', (snapshot) => {
-
-      var mapUsernameToUID = snapshot.child("mapUsernameToUID").val();
-      var UIDofUserIAmViewing = mapUsernameToUID[this.username];
-
-      var usersTheUserIsFollowing = snapshot.child("users").child(UIDofUserIAmViewing).child("following").val();
-      var followersTheUserHas = snapshot.child("users").child(UIDofUserIAmViewing).child("followers").val();
-
-      document.getElementById('followers').innerHTML = followersTheUserHas;
-      document.getElementById('following').innerHTML = usersTheUserIsFollowing;
-
-    });
-  }
-
-  getFollowedTopics() {
-    firebase.database().ref().once('value', (snapshot) => {
-      var mapUsernameToUID = snapshot.child("mapUsernameToUID").val();
-      var currentUser = mapUsernameToUID[this.username];
-
-      var followedTopics = snapshot.child("users").child(currentUser).child("followedTopics").val();
-      alert(followedTopics);
-      document.getElementById("followedTopics").innerHTML = followedTopics;
-    });
-  }
-
-  //adds the follower I am viewing to the list of users I follow AND
-  //adds me as a follower to the user I am viewing
-  followUserIAmViewing(){
-    firebase.database().ref().once('value', (snapshot) => {
-      var usersIAmFollowing = snapshot.child("users").child(firebase.auth().currentUser.uid).child("following").val();
-      var mapUsernameToUID = snapshot.child("mapUsernameToUID").val();
-      var UIDofUserIfollowed = mapUsernameToUID[this.username];
-      var followersOfUserIamFollowing = snapshot.child("users").child(UIDofUserIfollowed).child("followers").val();
-
-      var mapUIDToUsername = snapshot.child("mapUIDtoUsername").val();
-      var myUsername = mapUIDToUsername[firebase.auth().currentUser.uid];
-
-      if (mapUsernameToUID[this.username] != firebase.auth().currentUser.uid) {
-        if (usersIAmFollowing == null) {
-          firebase.database().ref().child("users").child(firebase.auth().currentUser.uid).child("following").set(this.username);
-        } else {
-          usersIAmFollowing = usersIAmFollowing + ", " + this.username;
-          firebase.database().ref().child("users").child(firebase.auth().currentUser.uid).child("following").set(usersIAmFollowing);
-        }
->>>>>>> rohangupta
 
         this.editMode = false;
         this.setState({
