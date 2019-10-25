dd958e2294672348a4cec496a093b43fbfb3c52b
Gosseling de Vries <gdevrie@purdue.edu>
2019-10-25 17:02:56 -0400

    .

--- twisster/src/TopBar/TopBarView.js
+++ twisster/src/TopBar/TopBarView.js
@@ -5,12 +5,8 @@ const TopBarTwisster = ({goLogout, goToProfile}) => {
     return (
       <div className="inner-body">
         <label className="top-bar-twisster" >Twistter</label>
-<<<<<<< HEAD
         <button className="top-bar-logout" onClick={goLogout}>Logout</button>
         <button className="top-bar-profile" onClick={goToProfile}>Profile</button>
-=======
-        <button id="logout" className="top-bar-logout" onClick={onClick}>Logout</button>
->>>>>>> 38286559cff1360c5cc2e59aefee51927c8e9ab3
         {/* <input className="top-bar-search" placeholder="Search"/>
         <button className="top-bar-button" type="button" onClick={onClickProfile}>Go to your profile</button>  */}   
       </div>
