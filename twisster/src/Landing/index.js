f0d1466bbb7568e40b8cc66c7192c64abe5478ee
Gosseling de Vries <gdevrie@purdue.edu>
2019-10-25 16:53:53 -0400

    .

--- twisster/src/Landing/index.js
+++ twisster/src/Landing/index.js
@@ -41,63 +41,7 @@ class Landing extends Component {
     
     async componentDidMount() {
 
-<<<<<<< HEAD
         var loggedIn = true;
-=======
-    window.setTimeout(() => {
-      this.updateMicroblogsList();
-    }, 1000);
-  }
-
-  updateMicroblogsList() {
-    //fetches the latest list of microblogs
-    this.state.users = []; //erase previous list of microblogs and re-fetch them from server and populate the page
-    
-    firebase.database().ref().once('value', (snapshot) => {
-      var mapUIDtoUsername = snapshot.child("mapUIDtoUsername").val();
-      var usernameOfUser = mapUIDtoUsername[firebase.auth().currentUser.uid];
-      var Microblogs = snapshot.child("users").child(firebase.auth().currentUser.uid).child("Microblogs").val();
-
-      if (Microblogs != null) {
-        for (var i = 0; i < Microblogs.length; i++) {
-          this.getUser(usernameOfUser, Microblogs[i].content, Microblogs[i].topics);
-        }
-      }
-    });
-  }
-
-  //get list of microblogs when page first loads
-  async componentDidMount() {
-    
-    //verify user is logged in before displaying page
-    firebase.auth().onAuthStateChanged((user) => {
-      console.log(user);
-      if (!user) {
-        this.props.history.push({
-          pathname: "/login"
-        });
-        return (null);
-      }
-
-      this.getMicroblogsForCurrentUser();
-    });
-  }
-
-  componentWillMount() {
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-
-        //CHANGE WHEN USER HAS USERNAME
-        this.loggedIn = true;
-
-        firebase.database().ref().once('value', (snapshot) => {
-          var UIDtoUsername = snapshot.child('mapUIDtoUsername').val();
-          var username = UIDtoUsername[user.uid];
-
-          this.userData = new UserData(username, this.loggedIn);
-          this.email = user.email;
-        });
->>>>>>> 38286559cff1360c5cc2e59aefee51927c8e9ab3
 
         var user = firebase.auth().currentUser;
         this.userData = await helperfunctions.getUserdataOfUser(user.uid, loggedIn);
