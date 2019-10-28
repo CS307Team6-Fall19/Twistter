eaa7230ebc5665d9f3247b8769071dd41398137d
Benjamin Loisch <ben.loisch@gmail.com>
2019-10-27 22:48:04 -0400

    delete user information from firebase database as well as auth

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -11,8 +11,19 @@ functions.pubsub.schedule('every 1 minutes').onRun((context) => {
     auth.listUsers().then((userRecords) => {
         userRecords.users.forEach((user) => {
             if (!user.emailVerified) {
-                admin.auth().deleteUser(user.uid).then(function() {
-                    console.log("deleted user " + user.email + " because unverified");
+
+                ref = admin.database().ref();
+                ref.once('value').then((snapshot) => {
+                    var mapUIDtoUsername = snapshot.child("mapUIDtoUsername").val();
+                    var username = mapUIDtoUsername[user.uid];
+                    admin.database().ref().child("mapUIDtoUsername").child(user.uid).remove();
+                    admin.database().ref().child("mapUsernameToEmail").child(username).remove();
+                    admin.database().ref().child("mapUsernameToUID").child(username).remove();
+                    admin.database().ref().child("users").child(user.uid).remove();
+
+                    admin.auth().deleteUser(user.uid).then(function() {
+                        console.log("deleted user " + user.email + " because unverified");
+                    });
                 });
             }
         });
