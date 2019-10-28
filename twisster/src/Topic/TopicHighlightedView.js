b1566fed8c1743c0c4f4b042983c6f39445b9588
Gosseling de Vries <gdevrie@purdue.edu>
2019-10-27 23:07:39 -0400

    Views for siplaying topics

---
+++ twisster/src/Topic/TopicHighlightedView.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Landing.css'
+
+const TopicHighlightedView = (props) => {
+    return(
+      <div className="topic-highlighted">
+        {props.text}
+      </div>
+    )
+}
+export default TopicHighlightedView 
