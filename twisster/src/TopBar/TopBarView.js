1309ac116714152255993e93d44a427894037779
Gosseling de Vries <gdevrie@purdue.edu>
2019-10-28 21:23:42 -0400

    .

--- twisster/src/TopBar/TopBarView.js
+++ twisster/src/TopBar/TopBarView.js
@@ -12,14 +12,10 @@ const TopBarTwisster = ({ goLogout, goToProfile, goToChat }) => {
       <button className="top-bar-profile" id="profile" onClick={goToProfile}>
         Profile
       </button>
-<<<<<<< HEAD
-      <button className="top-bar-chat" id="chat" onClick={goToChat}>Chat</button>
-=======
       <button className="top-bar-chat" id="chat" onClick={goToChat}>
         {/* <img className="picture" src={logo} alt="my image" onClick={goToChat} /> */}
         Chat
       </button>
->>>>>>> master
       {/* <input className="top-bar-search" placeholder="Search"/>
         <button className="top-bar-button" type="button" onClick={onClickProfile}>Go to your profile</button>  */}
     </div>
