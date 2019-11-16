fcdd5db765ad1030e344c11b86f9e8c29f346374
Siddharth Singh <singh315@purdue.edu.com>
2019-11-16 18:55:06 -0500

    Profile View Bedore merge

--- twisster/src/Landing/index.js
+++ twisster/src/Landing/index.js
@@ -7,84 +7,85 @@ import firebase from "firebase";
 import TopBar from "../TopBar";
 import Microblogs from "../Microblogs";
 
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { resolve } from "q";
 
 class Landing extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
+    this.state = {
+      loaded: false
+    };
 
-        super(props);        
+    this.editMode = false;
 
+    this.microblogPosted = this.microblogPosted.bind(this);
+  }
 
-        this.state = {
+  async microblogPosted() {
+    await this.updateMicroblogs();
+    this.setState({ loaded: true });
+  }
 
-            loaded: false
-        }
-        
-        this.editMode = false;
+  async updateMicroblogs() {
+    toast("Microblog posted!");
+    this.microblogs = await helperfunctions.getMicroblogsForUserTimeline(
+      this.userData.username
+    );
+    resolve("done");
+  }
 
-        this.microblogPosted = this.microblogPosted.bind(this);
-    }
-
-    async microblogPosted(){
-        await this.updateMicroblogs();
-        this.setState({loaded : true});
-    }
-
-    async updateMicroblogs(){
-        toast("Microblog posted!");
-        this.microblogs = await helperfunctions.getMicroblogsForUserTimeline(this.userData.username);
-        resolve("done");
-    }
-    
-    async componentDidMount() {
-
-        //verify user is logged in before displaying page
-        firebase.auth().onAuthStateChanged(async (user) => {
-            if (!user) {
-            this.props.history.push({
-                pathname: "/login"
-            });
-                return (null);
-            } else {
-                var loggedIn = true;
-                var user = firebase.auth().currentUser;
-                this.userData = await helperfunctions.getUserdataOfUser(user.uid, loggedIn);
-                
-            
-                this.microblogs = await helperfunctions.getMicroblogsForUserTimeline(this.userData.username);
-                this.setState({loaded : true});
-            }
-        });
-    }
-
-    goToProfile(){
+  async componentDidMount() {
+    //verify user is logged in before displaying page
+    firebase.auth().onAuthStateChanged(async user => {
+      if (!user) {
         this.props.history.push({
-          pathname: "/profile",
-          state: { userData: this.userData }
-        })
+          pathname: "/login"
+        });
+        return null;
+      } else {
+        var loggedIn = true;
+        var user = firebase.auth().currentUser;
+        this.userData = await helperfunctions.getUserdataOfUser(
+          user.uid,
+          loggedIn
+        );
+
+        this.microblogs = await helperfunctions.getMicroblogsForUserTimeline(
+          this.userData.username
+        );
+        this.setState({ loaded: true });
+      }
+    });
+  }
+
+  goToProfile() {
+    this.props.history.push({
+      pathname: "/profile",
+      state: { userData: this.userData }
+    });
+  }
+
+  render() {
+    if (this.state.loaded) {
+      return (
+        <div>
+          <TopBar userData={this.userData} />
+          <MicroblogWriter
+            username={this.userData.username}
+            microblogPosted={this.microblogPosted}
+          />
+          <Microblogs
+            microblogs={this.microblogs}
+            username={this.userData.username}
+          />
+        </div>
+      );
+    } else {
+      return null;
     }
-
-    render() {
-
-        if(this.state.loaded){
-            
-            return(
-                <div>
-                    <TopBar userData={this.userData}/>
-                    <MicroblogWriter username={this.userData.username} microblogPosted={this.microblogPosted}/>
-                    <Microblogs microblogs={this.microblogs} username={this.userData.username} />                               
-                </div>
-                
-            );
-
-        } else{
-            return  null;
-        }
-        
-    }
-
+  }
 }
-export default withRouter(Landing); 
+export default withRouter(Landing);
