42cb594c0e5821c9525154364501a4601db9f94c
Benjamin Loisch <ben.loisch@gmail.com>
2019-11-23 21:52:06 -0500

    firebase web hosting for hosting website

---
+++ twisster/build/precache-manifest.704ecc1b7606e46b052922b96ed4daf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290dc581258a42eef46026bf1027e5c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9a18fdfe6b77cf72915",
+    "url": "/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "c2c4d23ae2d5f97944b8",
+    "url": "/static/css/main.28e2d9d6.chunk.css"
+  },
+  {
+    "revision": "f9a18fdfe6b77cf72915",
+    "url": "/static/js/2.a8df0add.chunk.js"
+  },
+  {
+    "revision": "c2c4d23ae2d5f97944b8",
+    "url": "/static/js/main.44c53c81.chunk.js"
+  },
+  {
+    "revision": "aab618e3bfe4eb2dad8a",
+    "url": "/static/js/runtime~main.172dbb30.js"
+  },
+  {
+    "revision": "d26fb7f2bb22554d4f67aa99e200adec",
+    "url": "/static/media/twisster_logo.d26fb7f2.png"
+  }
+]);
