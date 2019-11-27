7309f699d82b440e3e4c04b6963d2397aae150bf
Siddharth Singh <singh315@purdue.edu.com>
2019-11-27 13:50:56 -0500

    Fixed merge issues. Stable build

--- twisster/src/Landing/index.js
+++ twisster/src/Landing/index.js
@@ -6,14 +6,13 @@ import helperfunctions from "../helperfunctions";
 import firebase from "firebase";
 import TopBar from "../TopBar";
 import Microblogs from "../Microblogs";
-import AutoCompleteSearchBar from "../AutoCompleteSearchBar/AutoCompleteSearchBar"
+import AutoCompleteSearchBar from "../AutoCompleteSearchBar/AutoCompleteSearchBar";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { resolve } from "q";
 
 class Landing extends Component {
-<<<<<<< HEAD
   constructor(props) {
     super(props);
 
@@ -24,6 +23,7 @@ class Landing extends Component {
     this.editMode = false;
 
     this.microblogPosted = this.microblogPosted.bind(this);
+    this.getAllUsernames = this.getAllUsernames.bind(this);
   }
 
   async microblogPosted() {
@@ -70,11 +70,32 @@ class Landing extends Component {
     });
   }
 
+  getAllUsernames() {
+    var user_list = [];
+    const promise = firebase
+      .database()
+      .ref()
+      .once("value", snapshot => {
+        var all_users = snapshot.child("mapUsernameToEmail").val();
+        var key;
+        for (key in all_users) {
+          if (all_users[key] != firebase.auth().currentUser.email) {
+            user_list.push(key);
+          }
+        }
+      });
+    return user_list;
+  }
+
   render() {
     if (this.state.loaded) {
       return (
         <div>
           <TopBar userData={this.userData} />
+          <AutoCompleteSearchBar
+            items={this.getAllUsernames()}
+            username={this.userData.username}
+          />
           <MicroblogWriter
             username={this.userData.username}
             microblogPosted={this.microblogPosted}
@@ -87,96 +108,8 @@ class Landing extends Component {
       );
     } else {
       return null;
-=======
-
-    constructor(props) {
-
-        super(props);        
-
-
-        this.state = {
-
-            loaded: false
-        }
-        
-        this.editMode = false;
-
-        this.microblogPosted = this.microblogPosted.bind(this);
-        this.getAllUsernames = this.getAllUsernames.bind(this);
-    }
-
-    async microblogPosted(){
-        await this.updateMicroblogs();
-        this.setState({loaded : true});
-    }
-
-    async updateMicroblogs(){
-        toast("Microblog posted!");
-        this.microblogs = await helperfunctions.getMicroblogsForUserTimeline(this.userData.username);
-        resolve("done");
-    }
-    
-    async componentDidMount() {
-
-        //verify user is logged in before displaying page
-        firebase.auth().onAuthStateChanged(async (user) => {
-            if (!user) {
-            this.props.history.push({
-                pathname: "/login"
-            });
-                return (null);
-            } else {
-                var loggedIn = true;
-                var user = firebase.auth().currentUser;
-                this.userData = await helperfunctions.getUserdataOfUser(user.uid, loggedIn);
-                
-            
-                this.microblogs = await helperfunctions.getMicroblogsForUserTimeline(this.userData.username);
-                this.setState({loaded : true});
-            }
-        });
-    }
-
-    goToProfile(){
-        this.props.history.push({
-          pathname: "/profile",
-          state: { userData: this.userData }
-        })
-    }
-
-    getAllUsernames(){
-        var user_list = [];
-        const promise = firebase.database().ref().once('value', (snapshot)=> {
-            var all_users = snapshot.child("mapUsernameToEmail").val();
-            var key;
-            for (key in all_users) {
-                if (all_users[key] != firebase.auth().currentUser.email) {
-                    user_list.push(key);
-                }
-            }
-        });
-        return user_list;
-    }
-
-    render() {
-
-        if(this.state.loaded){
-            return(
-                <div>
-                    <TopBar userData={this.userData}/>
-                    <AutoCompleteSearchBar items={this.getAllUsernames()} username={this.userData.username} />
-                    <MicroblogWriter username={this.userData.username} microblogPosted={this.microblogPosted}/>
-                    <Microblogs microblogs={this.microblogs} username={this.userData.username} />                               
-                </div>
-                
-            );
-
-        } else{
-            return  null;
-        }
-        
->>>>>>> master
     }
   }
 }
+
 export default withRouter(Landing);
